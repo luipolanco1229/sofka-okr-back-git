44e09574249750765995a3cb1b59ae1ad5b85ea3
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-27 06:31:44 -0500

    Se agregó la notacion @Repository en los repos de Usuario y Kr

--- src/main/java/co/com/sofka/okrs/repository/RepositoryKr.java
+++ src/main/java/co/com/sofka/okrs/repository/RepositoryKr.java
@@ -2,6 +2,8 @@ package co.com.sofka.okrs.repository;
 
 import co.com.sofka.okrs.domain.Kr;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositoryKr extends ReactiveMongoRepository<Kr, String> {
 }
--- src/main/java/co/com/sofka/okrs/repository/UsuarioRepository.java
+++ src/main/java/co/com/sofka/okrs/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package co.com.sofka.okrs.repository;
 
 import co.com.sofka.okrs.domain.Usuario;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends ReactiveMongoRepository<Usuario, String> {
 }
