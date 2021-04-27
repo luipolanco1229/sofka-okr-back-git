a627f75af66d9021c367d274a48da4e48b909928
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-27 16:51:31 -0500

    Se agregó la firma para filtrar por usuario y ordenarlo por avance del OKR

--- src/main/java/co/com/sofka/okrs/repository/RepositoryOKR.java
+++ src/main/java/co/com/sofka/okrs/repository/RepositoryOKR.java
@@ -3,8 +3,10 @@ package co.com.sofka.okrs.repository;
 import co.com.sofka.okrs.domain.Okr;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface RepositoryOKR extends ReactiveMongoRepository<Okr, String>  {
 
+    Flux<Okr> findByUsuarioIdOrderByAvanceOkrDesc(String responsable);
 }
