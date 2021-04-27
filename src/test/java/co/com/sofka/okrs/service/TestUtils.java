2a97c32654b8ac842763662a41828ee74ab8358d
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-27 11:12:40 -0500

    Se agregó una clase auxiliar para generar los mocks

--- src/test/java/co/com/sofka/okrs/service/TestUtils.java
+++ src/test/java/co/com/sofka/okrs/service/TestUtils.java
@@ -1,2 +1,11 @@
-package co.com.sofka.okrs.service;public class TestUtils {
+package co.com.sofka.okrs.service;
+
+import co.com.sofka.okrs.domain.Usuario;
+import reactor.core.publisher.Mono;
+
+public class TestUtils {
+
+    public static Mono<Usuario> usuarioFiltrado(){
+        return Mono.just(new Usuario("xxxx", "Daniel Burgos", "danielburgos@example.com"));
+    }
 }
