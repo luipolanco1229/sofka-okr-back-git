bad90ed804df80a8a6331ec1602986d3edbc6879
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-26 20:06:16 -0500

    Objeto de valor HistoricoAvance agregado

--- src/main/java/co/com/sofka/okrs/domain/HistoricoAvance.java
+++ src/main/java/co/com/sofka/okrs/domain/HistoricoAvance.java
@@ -1,2 +1,30 @@
-package co.com.sofka.okrs.domain;public class HistoricoAvance {
+package co.com.sofka.okrs.domain;
+
+import java.util.Date;
+
+public class HistoricoAvance {
+
+    private Date fechaActualizacion;
+    private Float nuevoAvance;
+
+    public HistoricoAvance(Date fechaActualizacion, Float nuevoAvance) {
+        this.fechaActualizacion = fechaActualizacion;
+        this.nuevoAvance = nuevoAvance;
+    }
+
+    public Date getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(Date fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public Float getNuevoAvance() {
+        return nuevoAvance;
+    }
+
+    public void setNuevoAvance(Float nuevoAvance) {
+        this.nuevoAvance = nuevoAvance;
+    }
 }
