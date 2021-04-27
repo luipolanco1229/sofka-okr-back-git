a44e3123732238b0b47cff8be2db0d86cb849cfa
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-26 20:05:31 -0500

    Entidad Usuario agregada

--- src/main/java/co/com/sofka/okrs/domain/Usuario.java
+++ src/main/java/co/com/sofka/okrs/domain/Usuario.java
@@ -1,2 +1,43 @@
-package co.com.sofka.okrs.domain;public class Usuario {
+package co.com.sofka.okrs.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Usuario {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String correo;
+
+    public Usuario(String id, String nombre, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
