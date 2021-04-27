f2d946f91c0617e47fe4a94e5e8e12e9dca371d3
@sebas.davila.garcia <sebas.davila.garcia@gmail.com>
2021-04-27 12:24:29 -0500

    implementacion del controlador, se agregaron las dependencias y las credenciales

--- src/main/java/co/com/sofka/okrs/controller/calendar/CalendarController.java
+++ src/main/java/co/com/sofka/okrs/controller/calendar/CalendarController.java
@@ -1,4 +1,28 @@
 package co.com.sofka.okrs.controller.calendar;
 
+import com.google.api.services.calendar.model.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+@RestController
+@RequestMapping("calendar")
 public class CalendarController {
+@Autowired
+CalendarService calendarService;
+
+    @PostMapping
+    public Mono<EventCalendar> guardar(@RequestBody Evento evento) throws IOException, GeneralSecurityException {
+       return calendarService.save(evento);
+    }
+
+    @GetMapping
+    public Flux<Event> leer() throws IOException, GeneralSecurityException {
+        return calendarService.load();
+    }
+
 }
