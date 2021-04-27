127c4873eefa2eed6f122efe80605f124bfa3599
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-27 08:54:59 -0500

    Se agregó una clase auxiliar assembler con un método que genera UsuarioVista a partir de un usuario

--- src/main/java/co/com/sofka/okrs/utils/Assembler.java
+++ src/main/java/co/com/sofka/okrs/utils/Assembler.java
@@ -1,2 +1,11 @@
-package co.com.sofka.okrs.utils;public class Assembler {
+package co.com.sofka.okrs.utils;
+
+import co.com.sofka.okrs.dashboard_dto.UsuarioVista;
+import co.com.sofka.okrs.domain.Usuario;
+
+public class Assembler {
+
+    public static UsuarioVista generarUsuarioVista(Usuario usuario){
+        return new UsuarioVista(usuario.getNombre(), usuario.getCorreo());
+    }
 }
