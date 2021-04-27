cf6e5b08d075686c49df2c52c096e4561bcb5278
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-27 08:56:47 -0500

    Se agregan test para el metodo generarUsuarioVista

--- src/test/java/co/com/sofka/okrs/utils/AssemblerTest.java
+++ src/test/java/co/com/sofka/okrs/utils/AssemblerTest.java
@@ -1,4 +1,23 @@
+package co.com.sofka.okrs.utils;
+
+import co.com.sofka.okrs.dashboard_dto.UsuarioVista;
+import co.com.sofka.okrs.domain.Usuario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AssemblerTest {
-  
+
+    @Test
+    public void generarUsuarioVista(){
+
+        Usuario usuario = new Usuario("xxxx", "Daniel Alejandro", "danielburgos@example.com");
+        UsuarioVista usuarioVistaEsperado = new UsuarioVista("Daniel Alejandro", "danielburgos@example.com");
+
+        UsuarioVista usuarioVistaActual = Assembler.generarUsuarioVista(usuario);
+
+        Assertions.assertEquals(usuarioVistaActual, usuarioVistaEsperado);
+    }
+
 }
