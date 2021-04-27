b18c539eee2325972136ee43e6da2b118b282799
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-27 11:10:52 -0500

    Se agregó una excepción cuando el resultado del query es vacio

--- src/main/java/co/com/sofka/okrs/service/ServicioDashboard.java
+++ src/main/java/co/com/sofka/okrs/service/ServicioDashboard.java
@@ -1,8 +1,6 @@
 package co.com.sofka.okrs.service;
 
 import co.com.sofka.okrs.dashboard_dto.UsuarioVista;
-import co.com.sofka.okrs.repository.RepositoryKr;
-import co.com.sofka.okrs.repository.RepositoryOKR;
 import co.com.sofka.okrs.repository.UsuarioRepository;
 import co.com.sofka.okrs.utils.Assembler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +11,11 @@ import reactor.core.publisher.Mono;
 public class ServicioDashboard {
 
     @Autowired
-    private RepositoryOKR repositoryOKR;
-
-    @Autowired
-    private RepositoryKr repositoryKr;
-
-    @Autowired
     private UsuarioRepository usuarioRepository;
 
     public Mono<UsuarioVista> usuarioPorId(String id){
-        return usuarioRepository.findById(id).map(Assembler::generarUsuarioVista);
+        return usuarioRepository.findById(id).map(Assembler::generarUsuarioVista)
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("El usuario no se encuentra registrado")));
     }
 
 }
