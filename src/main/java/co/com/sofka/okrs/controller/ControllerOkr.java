c18ceb234a07747b07635ce9126a9c4eef781ba5
Jhovan Espinal <jhovan1028@gmail.com>
2021-04-28 10:11:26 -0500

    se crea ControllerOkr

--- src/main/java/co/com/sofka/okrs/controller/ControllerOkr.java
+++ src/main/java/co/com/sofka/okrs/controller/ControllerOkr.java
@@ -13,14 +13,24 @@ public class ControllerOkr {
     @Autowired
     private ServiceOkr userService;
 
-    @PostMapping
-    public Mono<Okr> save(@RequestBody Okr okr){
-        return  userService.save(okr);
-    }
-
     @GetMapping
     public Flux<Okr> findAll() {
         return userService.findAll();
     }
 
-}
+    @PostMapping("/postOkr")
+    public Mono<Okr> save(@RequestBody Okr okr){
+        return  userService.save(okr);
+    }
+
+    @PutMapping("/updOkr")
+    public Mono<Okr> update(@RequestBody Okr okr){
+        return  userService.save(okr);
+
+//    @DeleteMapping("/delete{id}")
+//    public Mono<Void> delete(@PathVariable("id") String id){
+//        return userService.delete(id);
+    }
+    }
+
+
