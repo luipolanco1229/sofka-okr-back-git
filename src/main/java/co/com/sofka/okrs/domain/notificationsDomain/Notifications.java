ca39bee96a18286284f99d36838a3da104af0894
luipolanco1229 <Luipolanco1229>
2021-04-29 09:36:35 -0500

    Se creó los getters y setters del dominio de notifications

--- src/main/java/co/com/sofka/okrs/domain/notificationsDomain/Notifications.java
+++ src/main/java/co/com/sofka/okrs/domain/notificationsDomain/Notifications.java
@@ -1,10 +1,12 @@
 package co.com.sofka.okrs.domain.notificationsDomain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Notifications {
 
+        @Id
         String id;
         String userId;
         String notificationDescription;
@@ -12,7 +14,60 @@ public class Notifications {
         String krId;
         Boolean viewed;
 
+        public Notifications(String id, String userId, String notificationDescription, String okrId, String krId, Boolean viewed) {
+                this.id = id;
+                this.userId = userId;
+                this.notificationDescription = notificationDescription;
+                this.okrId = okrId;
+                this.krId = krId;
+                this.viewed = viewed;
+        }
 
+        public String getId() {
+                return id;
+        }
 
+        public void setId(String id) {
+                this.id = id;
+        }
 
+        public String getUserId() {
+                return userId;
+        }
+
+        public void setUserId(String userId) {
+                this.userId = userId;
+        }
+
+        public String getNotificationDescription() {
+                return notificationDescription;
+        }
+
+        public void setNotificationDescription(String notificationDescription) {
+                this.notificationDescription = notificationDescription;
+        }
+
+        public String getOkrId() {
+                return okrId;
+        }
+
+        public void setOkrId(String okrId) {
+                this.okrId = okrId;
+        }
+
+        public String getKrId() {
+                return krId;
+        }
+
+        public void setKrId(String krId) {
+                this.krId = krId;
+        }
+
+        public Boolean getViewed() {
+                return viewed;
+        }
+
+        public void setViewed(Boolean viewed) {
+                this.viewed = viewed;
+        }
 }
