23cd44fb3c1f9c55128b203c75d1dc47aae8bccf
danielburgosr96 <danielburgosr96@gmail.com>
2021-04-29 18:03:54 -0500

    Se agregó una clase auxiliar que genera los krs y okrs que son mockeados en los test

--- src/test/java/co/com/sofka/okrs/testHelpers/dashboardTestHelpers/TestHelpersDashboard.java
+++ src/test/java/co/com/sofka/okrs/testHelpers/dashboardTestHelpers/TestHelpersDashboard.java
@@ -1,2 +1,80 @@
-package co.com.sofka.okrs.testHelpers.dashboardTestHelpers;public class TestHelpersDashboard {
+package co.com.sofka.okrs.testHelpers.dashboardTestHelpers;
+
+import co.com.sofka.okrs.domain.HistoricalAdvance;
+import co.com.sofka.okrs.domain.Kr;
+import co.com.sofka.okrs.domain.Okr;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class TestHelpersDashboard {
+
+    public static List<HistoricalAdvance> generateDates(){
+        HistoricalAdvance advance1 = new HistoricalAdvance("xxx", 0f);
+        advance1.setDateUpdate(new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime());
+        HistoricalAdvance advance2 = new HistoricalAdvance("xxx", 17f);
+        advance2.setDateUpdate(new GregorianCalendar(2020, Calendar.FEBRUARY, 2).getTime());
+        HistoricalAdvance advance3 = new HistoricalAdvance("xxx", 20f);
+        advance3.setDateUpdate(new GregorianCalendar(2020, Calendar.APRIL, 1).getTime());
+        HistoricalAdvance advance4 = new HistoricalAdvance("xxx", 40f);
+        advance4.setDateUpdate(new GregorianCalendar(2020, Calendar.AUGUST, 1).getTime());
+
+        return List.of(advance1, advance2, advance3, advance4);
+    }
+
+    public static Okr generate_okr(){
+        return  new Okr("xxx",
+                "Prueba 1",
+                "Que se ejecute la prueba",
+                "Daniel Burgos",
+                "danielburgos@ejemplo.com",
+                "12",
+                "Se espera que se ejecute la prueba",
+                "Desarrollo",
+                80f,
+                generateDates());
+    }
+
+    public static Kr generate_kr1(){
+        return new Kr("xxx-1",
+                "Kr Prueba 1",
+                "Xxxx",
+                "Daniel Burgos",
+                "danielburgos@ejemplo.com",
+                new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime(),
+                new GregorianCalendar(2020, Calendar.JULY, 1).getTime(),
+                20f,
+                40f,
+                "xxxxxx-description"
+        );
+    }
+
+    public static Kr generate_kr2(){
+        return new Kr("xxx-2",
+                "Kr Prueba 2",
+                "Xxxx",
+                "Daniel Burgos",
+                "danielburgos@ejemplo.com",
+                new GregorianCalendar(2020, Calendar.FEBRUARY, 1).getTime(),
+                new GregorianCalendar(2020, Calendar.AUGUST, 1).getTime(),
+                20f,
+                40f,
+                "xxxxxx-description"
+        );
+    }
+
+    public static Kr generate_kr3(){
+        return new Kr("xxx-3",
+                "Kr Prueba 3",
+                "Xxxx",
+                "Daniel Burgos",
+                "danielburgos@ejemplo.com",
+                new GregorianCalendar(2020, Calendar.MARCH, 1).getTime(),
+                new GregorianCalendar(2021, Calendar.JANUARY, 1).getTime(),
+                20f,
+                20f,
+                "xxxxxx-description"
+        );
+    }
 }
