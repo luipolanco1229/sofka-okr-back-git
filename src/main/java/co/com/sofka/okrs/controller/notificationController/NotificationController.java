1c6a65cce2cdeda5d0fd436d34fb41e01e1faf99
Andrés Hadechine <andreshadechine@gmail.com>
2021-04-29 09:40:45 -0500

    Creación del servicio y controlador de configuracion de notificaciones

--- src/main/java/co/com/sofka/okrs/controller/notificationController/NotificationController.java
+++ src/main/java/co/com/sofka/okrs/controller/notificationController/NotificationController.java
@@ -1,17 +1,27 @@
 package co.com.sofka.okrs.controller.notificationController;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import co.com.sofka.okrs.domain.User;
+import co.com.sofka.okrs.service.notificationService.NotificationConfigurationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController("/notifications")
 public class NotificationController {
+@Autowired
+    NotificationConfigurationService notificationConfigurationService;
 
     @GetMapping("/{id}")
     public Flux<String> completedOkr(@PathVariable String id){
         return Flux.empty();
     }
+
+    @GetMapping("/idConsultConfig/{id}")
+    public Mono<User> ConsultConfig(@PathVariable String id){ return notificationConfigurationService.ConsultConfig(id);}
+
+    @PostMapping("/idInsertConfig")
+    public Mono<User> InsertConfig(@RequestBody User user){return Mono.empty(); }
+
 }
--- src/main/java/co/com/sofka/okrs/service/notificationService/NotificationConfigurationService.java
+++ src/main/java/co/com/sofka/okrs/service/notificationService/NotificationConfigurationService.java
@@ -1,4 +1,28 @@
 package co.com.sofka.okrs.service.notificationService;
 
+import co.com.sofka.okrs.domain.User;
+import co.com.sofka.okrs.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
 public class NotificationConfigurationService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Mono<User> ConsultConfig (String id){
+        return userRepository.findById(id);
+    }
+
+    public Mono<User> InsertConfig (User user){
+
+        userRepository.insert(user);
+
+        return Mono.empty();
+    }
+
+
+
 }
